acdfbad9421fef34eeed91f28b8e4591f2d7cd37
Peter Mathia <mathiapeter@outlook.com>
2021-12-20 18:22:27 +0100

    sphere normals

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,27 @@ use std::fs;
 use raytrace::ray::Ray;
 
 fn color(r: &Ray) -> Vec3<f32> {
-    if hit_sphere(Vec3::<f32>::new(0.0,0.0,-1.0), 0.5, r) {
-        return Vec3::<f32>::new(1.0, 0.0, 0.0)
+    let t = hit_sphere(Vec3::<f32>::new(0.0,0.0,-1.0), 0.5, r);
+    if t > 0f32 {
+        let n = (r.point_at_parameter(t) - Vec3::<f32>::new(0.0,0.0,-1.0)).normalize();
+        return Vec3::<f32>::new(n.x+1.0, n.y+1.0, n.z+1.0)*0.5;
     }
     let unit_direction: Vec3<f32> = r.direction().normalize();
     let t: f32 = 0.5 * (unit_direction.y + 1.0);
     Vec3::<f32>::new(1.0,1.0,1.0) * (1.0-t) + Vec3::<f32>::new(0.5,0.7,1.0) * t
 }
 
-fn hit_sphere(center: Vec3<f32>, radius: f32, r: &Ray) -> bool {
+fn hit_sphere(center: Vec3<f32>, radius: f32, r: &Ray) -> f32 {
     let oc = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = oc.dot(r.direction()) * 2.0;
     let c = oc.dot(oc) - radius*radius;
     let discriminant = b*b - 4.0*a*c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1f32
+    } else {
+        (-b - discriminant.sqrt()) / (2f32*a)
+    }
 }
 
 fn main() {
