6231e53efc10d00fa4f0090f2b313977b15ac8e8
Peter Mathia <mathiapeter@outlook.com>
2021-12-20 19:57:47 +0100

    antialiasing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate math;
+extern crate rand;
+
 use math::vector::{ Vec3, Normalize };
 
 use std::fs;
-use raytrace::{ray::*, sphere::{Sphere, HitableList}, camera::Camera};
+use rand::distributions::{Distribution, Uniform};
 
+use raytrace::{ray::*, sphere::{Sphere, HitableList}, camera::Camera};
 fn color(r: &Ray, world: &dyn Hitable) -> Vec3<f32> {
     let mut rec = HitRecord::default();
     if world.hit(r, 0.0, f32::MAX, &mut rec) {
@@ -18,8 +21,9 @@ fn color(r: &Ray, world: &dyn Hitable) -> Vec3<f32> {
 fn main() {
     println!("Hello, world!");
 
-    let nx = 200u32;
-    let ny = 100u32;
+    let nx = 800u32;
+    let ny = 400u32;
+    let ns = 100u32;
 
     let mut data = String::new();
     data.push_str("P3\n");
@@ -38,12 +42,19 @@ fn main() {
     ];
     let list = HitableList::new(objects);
 
+    let mut rng = rand::thread_rng();
+    let die = Uniform::from(0..1000);
+    
     for j in (0..(ny)).rev() {
         for i in 0..nx {
-            let u = i as f32 / nx as f32;
-            let v = j as f32 / ny as f32;
-            let r = camera.get_ray(u, v);
-            let col = color(&r, &list);
+            let mut col = Vec3::<f32>::zero();
+            for _s in 0..ns {
+                let u = (i as f32 + die.sample(&mut rng) as f32 / 1000.0) / nx as f32;
+                let v = (j as f32 + die.sample(&mut rng) as f32 / 1000.0) / ny as f32;
+                let r = camera.get_ray(u, v);
+                col = col + color(&r, &list);
+            }
+            col = col / ns as f32;
             let ir = (255.99*col.x) as u8;
             let ig = (255.99*col.y) as u8;
             let ib = (255.99*col.z) as u8;
